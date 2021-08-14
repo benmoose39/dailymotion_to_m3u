7bfb91e087787e93d3fe94b18fb32bad8b379f76
benmoose39 <unnik1998@gmail.com>
2021-08-14 18:31:54 +0530

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo $(dirname $0)
 cd $(dirname $0)/scripts/
-python3 dailymotion_localrun.py > ../dailymotion.m3u
+python3 dailymotion_m3ugrabber.py > ../dailymotion.m3u
 echo m3u grabbed
