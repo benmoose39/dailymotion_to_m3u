efab027a9237352126e096e31f0b6dbd6e6f1319
benmoose39 <unnik1998@gmail.com>
2021-08-15 00:09:41 +0530

    Update dailymotion_m3ugrabber.py

--- scripts/dailymotion_m3ugrabber.py
+++ scripts/dailymotion_m3ugrabber.py
@@ -14,24 +14,35 @@ banner = r'''
 
 import requests
 import os
+import sys
+
+proxies = {}
+if len(sys.argv) == 2:
+    proxies = {
+                'http' : sys.argv[1],
+                'https' : sys.argv[1]
+              }
 
 na = 'https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/assets/moose_na.m3u'
 def grab(line):
     try:
         _id = line.split('/')[4]
-        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}').json()['qualities']['auto'][0]['url']
-        m3u = s.get(response).text
+        response = s.get(f'https://www.dailymotion.com/player/metadata/video/{_id}', proxies=proxies).json()['qualities']['auto'][0]['url']
+        m3u = s.get(response, proxies=proxies).text
         m3u = m3u.strip().split('\n')[1:]
         d = {}
         cnd = True
         for item in m3u:
             if cnd:
                 resolution = item.strip().split(',')[2].split('=')[1]
+                if resolution not in d:
+                    d[resolution] = []
             else:
-                d[resolution] = item
+                d[resolution]= item
             cnd = not cnd
+        #print(m3u)
         m3u = d[max(d, key=int)]    
-    except:
+    except Exception as e:
         m3u = na
     finally:
         print(m3u)
