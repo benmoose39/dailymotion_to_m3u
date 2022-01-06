24e927a56ef4d4cdef564019185b87d2d7630d13
benmoose39 <unnik1998@gmail.com>
2022-01-06 13:11:49 +0530

    Update autorun_github.sh

--- autorun_github.sh
+++ autorun_github.sh
@@ -2,6 +2,6 @@
 echo $(dirname $0)
 cd $(dirname $0)/scripts/
 python3 dailymotion_m3ugrabber.py > ../dailymotion-US.m3u
-python3 dailymotion_m3ugrabber.py "212.129.57.194:41258" > ../dailymotion-EU.m3u
+#python3 dailymotion_m3ugrabber.py "212.129.57.194:41258" > ../dailymotion-EU.m3u
 
 echo m3u grabbed
